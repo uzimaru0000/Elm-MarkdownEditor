1e37c1fb7f532620ace3b0a6f30bd57ecb0072d9
ShujiOba <shuji365630@gmail.com>
2018-01-01 01:14:42 +0900

    update

--- src/Elm/Main.elm
+++ src/Elm/Main.elm
@@ -1,13 +1,27 @@
 module Main exposing (..)
 
-import Html exposing (Html, program, text)
+import Html exposing (Html, program, textarea, div, text, input, header, iframe)
+import Html.Attributes exposing (type_, id, class, style)
+import Html.Events exposing (onInput)
+import Markdown exposing (toHtml)
+import Window exposing (Size, resizes)
 
 
 -- model
 
 
 type alias Model =
-    String
+    { plane : String
+    , windowSize : Size
+    }
+
+
+init : ( Model, Cmd Msg )
+init =
+    { plane = "hoge"
+    , windowSize = Size 0 0
+    }
+        ! []
 
 
 
@@ -16,6 +30,8 @@ type alias Model =
 
 type Msg
     = NoOp
+    | TextInput String
+    | ResizeWindow Size
 
 
 
@@ -24,7 +40,23 @@ type Msg
 
 view : Model -> Html Msg
 view model =
-    text model
+    div []
+        [ headerView
+        , contentView model
+        ]
+
+
+headerView : Html Msg
+headerView =
+    header [] []
+
+
+contentView : Model -> Html Msg
+contentView model =
+    div [ id "content", style [ ( "height", toString model.windowSize.height ++ "px" ) ] ]
+        [ div [] [ textarea [ onInput TextInput, id "planeText" ] [] ]
+        , toHtml [ id "mdArea" ] model.plane
+        ]
 
 
 
@@ -34,8 +66,23 @@ view model =
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
+        TextInput str ->
+            { model | plane = str } ! []
+
+        ResizeWindow s ->
+            { model | windowSize = s } ! []
+
         _ ->
             model ! []
+
+
+
+-- Subscriptions
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    resizes ResizeWindow
 
 
 
@@ -45,8 +92,8 @@ update msg model =
 main : Program Never Model Msg
 main =
     program
-        { init = "hoge" ! []
+        { init = init
         , view = view
         , update = update
-        , subscriptions = always Sub.none
+        , subscriptions = subscriptions
         }
